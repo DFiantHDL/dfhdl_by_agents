f1ceb564df64f39f00202f0749e0dfec05222071
Oron Port <oron.port@dfiant.works>
2026-04-22 08:11:46 +0300

    wip merging DFBoolOrBit into stubs

--- core/src/main/scala/dfhdl/core/stubs.scala
+++ core/src/main/scala/dfhdl/core/stubs.scala
@@ -1,5 +1,6 @@
 package dfhdl.core
 import scala.annotation.Annotation
+import ir.DFVal.Func.Op as FuncOp
 
 // stubs replacing dfhdl.compiler.ir and dfhdl.internals
 object ir:
@@ -96,3 +97,48 @@ protected trait DFCGLP:
 object DFCG extends DFCGLP:
   def apply(): DFCG = DFC.emptyNoEO
   given DFCG(using dfc: DFC): DFCG = dfc
+
+type BitNum = 0 | 1
+type DFBoolOrBit = DFType[ir.DFBoolOrBit, NoArgs]
+object DFBoolOrBit:
+  given DFBool = DFBool
+
+  object Val:
+    trait Candidate[R]:
+      type OutT <: DFBoolOrBit
+      type OutP
+    object Candidate:
+      type Types = DFValOf[DFBoolOrBit] | Boolean | BitNum
+      type Aux[R, T <: DFBoolOrBit, P] = Candidate[R] { type OutT = T; type OutP = P }
+    end Candidate
+
+    object Ops:
+      import DFVal.Ops.BoolOnlyOp
+      given evLogicOpDFBoolOrBit[
+          Op <: FuncOp.|.type | FuncOp.&.type | FuncOp.^.type,
+          L <: Candidate.Types,
+          LT <: DFBoolOrBit,
+          LP,
+          R <: Candidate.Types,
+          RT <: DFBoolOrBit,
+          RP
+      ](using
+          icL: Candidate.Aux[L, LT, LP],
+          icR: Candidate.Aux[R, RT, RP],
+          op: ValueOf[Op]
+      ): ExactOp2Aux[Op, DFC, DFValAny, L, R, DFValTP[LT, LP | RP]] = ???
+      given evLogicOpDFBoolOrBit2[
+          Op <: FuncOp.|.type | FuncOp.&.type,
+          L <: Candidate.Types,
+          R <: Candidate.Types,
+          O <: DFValAny
+      ](using
+          ic: ExactOp2Aux[Op, DFC, DFValAny, L, R, O]
+      ): ExactOp2Aux[BoolOnlyOp[Op], DFC, DFValAny, L, R, O] = ???
+
+    end Ops
+  end Val
+end DFBoolOrBit
+
+type DFBool = DFType[ir.DFBool.type, NoArgs]
+final lazy val DFBool = ir.DFBool.asFE[DFBool]
