1c9e60eb7932e477edf2a9b1b2a8f97c09fdce74
Oron Port <oron.port@dfiant.works>
2026-04-22 07:04:28 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/asFE.scala
+++ core/src/main/scala/dfhdl/core/asFE.scala
@@ -1,8 +1,3 @@
 package dfhdl.core
-import dfhdl.internals.*
-import dfhdl.compiler.ir
-
 export DFType.asFE
 
-given canEqualNothingL: CanEqual[Nothing, Any] = CanEqual.derived
-given canEqualNothingR: CanEqual[Any, Nothing] = CanEqual.derived
