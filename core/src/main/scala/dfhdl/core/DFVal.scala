378dd42c19e390826834efaee4a966d52aca9c60
Oron Port <oron.port@dfiant.works>
2026-04-22 09:54:38 +0300

    wip shortening macro to inline def x: Any = #{ ??? }

--- core/src/main/scala/dfhdl/core/DFVal.scala
+++ core/src/main/scala/dfhdl/core/DFVal.scala
@@ -6,7 +6,7 @@ type DFValAny = DFVal[DFType, ModifierAny]
 type DFConstOf[+T <: DFType] = DFVal[T, Modifier.CONST]
 type DFValTP[+T <: DFType, +P] = DFVal[T, Modifier[P]]
 
-inline def isConstCheck: Boolean = ${ ??? }
+inline def x: Any = ${ ??? }
 
 object DFVal:
   export DFXInt.Ops.{c1, c2}
