e8711f62bbea2823167394e1f83fcf1ffb60f315
Oron Port <oron.port@dfiant.works>
2026-04-22 06:56:40 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/Arg.scala
+++ core/src/main/scala/dfhdl/core/Arg.scala
@@ -2,28 +2,5 @@ package dfhdl.core
 import dfhdl.internals.*
 
 object Arg:
-  object Width
-      extends Check1[
-        Int,
-        [t <: Int] =>> t > 0,
-        [t <: Int] =>> "Width must be positive, but found: " + t
-      ]
-  object SignedWidth
-      extends Check1[
-        Int,
-        [t <: Int] =>> t > 1,
-        [t <: Int] =>> "Signed value width must be larger than 1, but found: " + t
-      ]
-  object Positive
-      extends Check1[
-        Int,
-        [t <: Int] =>> t > 0,
-        [t <: Int] =>> "Argument must be positive, but found: " + t
-      ]
-  object LargerThan1
-      extends Check1[
-        Int,
-        [t <: Int] =>> t > 1,
-        [t <: Int] =>> "Argument must be larger than 1, but found: " + t
-      ]
+  object Width extends Check1[Int, [t <: Int] =>> true, [t <: Int] =>> ""]
 end Arg
