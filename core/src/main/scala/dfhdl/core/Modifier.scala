cf43f56ef12ecb846a1288a1d16455f3c86b9d53
Oron Port <oron.port@dfiant.works>
2026-04-22 06:48:30 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/Modifier.scala
+++ core/src/main/scala/dfhdl/core/Modifier.scala
@@ -1,56 +1,14 @@
 package dfhdl.core
-import dfhdl.compiler.ir
-import ir.DFVal.Modifier as IRModifier
-import dfhdl.internals.*
 
-//A: Access, C: Connectivity, I: Initialization, P: Parameteric (Const or not)
-sealed class Modifier[+A, +C, +I, +P](val value: IRModifier):
-  override def toString: String = value.toString
-
+sealed class Modifier[+A, +C, +I, +P]
 type ModifierAny = Modifier[Any, Any, Any, Any]
 object Modifier:
   sealed trait Assignable
-  sealed trait AssignableREG
-  sealed trait AssignableSHARED extends Assignable
-  sealed trait Connectable
-  sealed trait Initializable
-  sealed trait Initialized
-  sealed trait Port
-  sealed trait PortIN extends Port
-  sealed trait PortOUT extends Port, Assignable
-  sealed trait PortINOUT extends PortIN, PortOUT
   type Mutable = Modifier[Assignable, Any, Any, dfhdl.core.NOTCONST]
-  type Dcl = Modifier[Assignable, Connectable, Initializable, dfhdl.core.NOTCONST]
-  type DclPort[+A] = Modifier[A, Connectable, Initializable, dfhdl.core.NOTCONST]
-  type DclREG[+A] = Modifier[AssignableREG & A, Connectable, Initializable, dfhdl.core.NOTCONST]
-  type DclSHARED = Modifier[AssignableSHARED, Any, Initializable, dfhdl.core.NOTCONST]
-  protected type RTDomainOnly[A] = AssertGiven[
-    A <:< DomainType.RT,
-    "`.REG` declaration modifier is only allowed under register-transfer (RT) domains."
-  ]
-  protected type EDDomainOnly[A] = AssertGiven[
-    A <:< DomainType.ED,
-    "`.SHARED` variable modifier is only allowed under event-driven (ED) domains."
-  ]
-  object VAR extends Dcl(IRModifier(IRModifier.VAR, IRModifier.Ordinary)):
-    protected object pREG extends DclREG[Any](IRModifier(IRModifier.VAR, IRModifier.REG))
-    inline def REG(using dt: DomainType)(using RTDomainOnly[dt.type]) = pREG
-    protected object pSHARED extends DclSHARED(IRModifier(IRModifier.VAR, IRModifier.SHARED))
-    inline def SHARED(using dt: DomainType)(using EDDomainOnly[dt.type]) = pSHARED
-  object IN extends DclPort[PortIN](IRModifier(IRModifier.IN, IRModifier.Ordinary))
-  object OUT extends DclPort[PortOUT](IRModifier(IRModifier.OUT, IRModifier.Ordinary)):
-    protected object pREG extends DclREG[PortOUT](IRModifier(IRModifier.OUT, IRModifier.REG))
-    inline def REG(using dt: DomainType)(using RTDomainOnly[dt.type]) = pREG
-  object INOUT extends DclPort[PortINOUT](IRModifier(IRModifier.INOUT, IRModifier.Ordinary))
+  type Dcl = Modifier[Assignable, Any, Any, dfhdl.core.NOTCONST]
   type CONST = Modifier[Any, Any, Any, dfhdl.core.CONST]
-  extension (modifier: ModifierAny) def asIR: IRModifier = modifier.value
-
 end Modifier
 
-sealed trait VAL
 sealed trait ISCONST[T <: Boolean]
 type CONST = ISCONST[true]
 type NOTCONST = Any
-sealed trait DFRET
-sealed trait RTRET
-sealed trait EDRET
