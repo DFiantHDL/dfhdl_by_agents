ba39738404c897f719a000207e5b0f68b1f2b596
Oron Port <oron.port@dfiant.works>
2026-04-22 05:33:02 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/Domain.scala
+++ core/src/main/scala/dfhdl/core/Domain.scala
@@ -1,46 +1,21 @@
 package dfhdl.core
-import dfhdl.internals.*
 import dfhdl.compiler.ir
-import dfhdl.compiler.printing.*
 import scala.reflect.ClassTag
 private[dfhdl] trait Domain extends Container with scala.reflect.Selectable:
   private[core] type TScope = DFC.Scope.Domain
   private[core] type TOwner = Domain.Block
   final protected given TScope = DFC.Scope.Domain
-  final private[dfhdl] def initOwner: TOwner =
-    Domain.Block(__domainType)
-  final override def onCreateEnd(thisOwner: Option[This]): Unit =
-    dfc.exitOwner()
+  final private[dfhdl] def initOwner: TOwner = ???
+  final override def onCreateEnd(thisOwner: Option[This]): Unit = ???
 
 object Domain:
   type Block = DFOwner[ir.DomainBlock]
   object Block:
-    def apply(domainType: ir.DomainType)(using DFC): Block = trydf:
-      dfc.owner.asIR match
-        case _: ir.DFDomainOwner =>
-        case _                   =>
-          throw new IllegalArgumentException(
-            "A domain can only be directly owned by a design, an interface, or another domain."
-          )
-      val ownerRef: ir.DFOwner.Ref =
-        dfc.ownerOption.map(_.asIR.ref).getOrElse(ir.DFMember.Empty.ref)
-      ir.DomainBlock(domainType, ownerRef, dfc.getMeta, dfc.tags).addMember.asFE
+    def apply(domainType: ir.DomainType)(using DFC): Block = ???
   end Block
   extension [D <: Domain](domain: D)
-    infix def tag[CT <: ir.DFTag: ClassTag](customTag: CT)(using dfc: DFC): D =
-      import dfc.getSet
-      domain.containedOwner.asIR
-        .setTags(_.tag(customTag))
-        .setMeta(m => if (m.isAnonymous && !dfc.getMeta.isAnonymous) dfc.getMeta else m)
-      domain
-    infix def setName(name: String)(using dfc: DFC): D =
-      import dfc.getSet
-      domain.containedOwner.asIR
-        .setMeta(m =>
-          if (m.isAnonymous && !dfc.getMeta.isAnonymous) dfc.getMeta.setName(name)
-          else m.setName(name)
-        )
-      domain
+    infix def tag[CT <: ir.DFTag: ClassTag](customTag: CT)(using dfc: DFC): D = ???
+    infix def setName(name: String)(using dfc: DFC): D = ???
   end extension
 
 end Domain
