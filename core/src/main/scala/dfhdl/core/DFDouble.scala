4c505afc53e136b679ef86f73b6fd466480e5d61
Oron Port <oron.port@dfiant.works>
2026-04-22 03:32:24 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFDouble.scala
+++ core/src/main/scala/dfhdl/core/DFDouble.scala
@@ -1,102 +1,12 @@
 package dfhdl.core
 import dfhdl.compiler.ir
-import ir.DFVal.Func.Op as FuncOp
-import dfhdl.internals.*
-import scala.annotation.targetName
 type DFDouble = TDFDouble
 final val DFDouble = ir.DFDouble.asFE[DFDouble]
 
-//using TDFDouble to have a companion object
 type TDFDouble = DFType[ir.DFDouble, NoArgs]
 object TDFDouble:
   given DFDouble = DFDouble
   object Val:
-    trait Candidate[R] extends Exact0.TC[R, DFC]:
-      type OutT = DFDouble
-      type OutP
-      type Out = DFValTP[OutT, OutP]
-      def conv(from: R)(using DFC): Out = apply(from)
-      def apply(arg: R)(using DFC): Out
-
-    object Candidate:
-      type Exact = Exact0[DFC, Candidate]
-      type Aux[R, P] = Candidate[R] { type OutP = P }
-      given fromDouble[R <: Double]: Candidate[R] with
-        type OutP = CONST
-        def apply(arg: R)(using DFC): Out =
-          DFVal.Const(DFDouble, Some(arg), named = true)
-      given fromDFDoubleVal[P, R <: DFValTP[DFDouble, P]]: Candidate[R] with
-        type OutP = P
-        def apply(arg: R)(using DFC): Out = arg
-      given fromIf[
-          C <: DFValOf[DFBoolOrBit],
-          T,
-          F,
-          TP,
-          FP,
-          R <: IfWrapper[C, T, F]
-      ](using
-          tTC: Candidate[T] { type OutP = TP },
-          fTC: DFVal.TC[DFDouble, F] { type OutP = FP }
-      ): Candidate[R] with
-        type OutP = TP | FP
-        def apply(value: R)(using DFC): Out = value.unwrap
-      end fromIf
-    end Candidate
-
-    object TC:
-      import DFVal.TC
-      given DFDoubleFromCandidate[
-          R,
-          RP,
-          IC <: Candidate[R]
-      ](using ic: IC { type OutP = RP }): TC[DFDouble, R] with
-        type OutP = RP
-        def conv(dfType: DFDouble, arg: R)(using DFC): Out = ic(arg)
-
-    object Compare:
-      import DFVal.Compare
-      given DFDoubleCompare[R, RP, IC <: Candidate[R], Op <: FuncOp, C <: Boolean](using
-          ic: IC { type OutP = RP },
-          op: ValueOf[Op],
-          castling: ValueOf[C]
-      ): Compare[DFDouble, R, Op, C] with
-        type OutP = RP
-        def conv(dfType: DFDouble, arg: R)(using DFC): Out = ic(arg)
-
-    object Ops:
-      given evOpArithDFDouble[
-          Op <: FuncOp.+.type | FuncOp.-.type | FuncOp.*.type | FuncOp./.type | FuncOp.max.type |
-            FuncOp.min.type,
-          LPA,
-          L <: DFValTP[DFDouble, LPA] | Double | IfWrapper[?, ?, ?],
-          LP,
-          RPA,
-          R <: DFValTP[DFDouble, RPA] | Double | IfWrapper[?, ?, ?],
-          RP
-      ](using
-          icL: Candidate.Aux[L, LP],
-          icR: Candidate.Aux[R, RP],
-          op: ValueOf[Op]
-      ): ExactOp2Aux[Op, DFC, DFValAny, L, R, DFValTP[DFDouble, LP | RP]] =
-        new ExactOp2[Op, DFC, DFValAny, L, R]:
-          type Out = DFValTP[DFDouble, LP | RP]
-          def apply(lhs: L, rhs: R)(using DFC): Out = trydf {
-            val dfcAnon = dfc.anonymize
-            val lhsVal = icL(lhs)(using dfcAnon)
-            val rhsVal = icR(rhs)(using dfcAnon)
-            DFVal.Func(DFDouble, op.value, List(lhsVal, rhsVal))
-          }(using dfc, CTName(op.value.toString))
-      end evOpArithDFDouble
-
-      extension [P](lhs: DFValTP[DFDouble, P])
-        def toScalaDouble(using DFC, DFVal.ConstCheck[P]): Double =
-          lhs.toScalaValue
-
-        def unary_-(using DFCG): DFValTP[DFDouble, P] = trydf {
-          DFVal.Func(DFDouble, FuncOp.unary_-, List(lhs))
-        }
-      end extension
-    end Ops
-  end Val
-end TDFDouble
+    object TC
+    object Compare
+    object Ops
