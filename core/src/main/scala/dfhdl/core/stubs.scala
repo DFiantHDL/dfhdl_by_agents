0ef7bebc2fc89c5d08aa5a45241c803c3d5aec42
Oron Port <oron.port@dfiant.works>
2026-04-22 09:11:24 +0300

    wip simplifying DFBoolOrBit Candidate and logic givens

--- core/src/main/scala/dfhdl/core/stubs.scala
+++ core/src/main/scala/dfhdl/core/stubs.scala
@@ -96,34 +96,19 @@ object DFBoolOrBit:
 
   object Val:
     trait Candidate[R]:
-      type OutT <: DFBoolOrBit
       type OutP
     object Candidate:
-      type Types = DFValOf[DFBoolOrBit] | Boolean | BitNum
-      type Aux[R, T <: DFBoolOrBit, P] = Candidate[R] { type OutT = T; type OutP = P }
+      type Aux[R, P] = Candidate[R] { type OutP = P }
     end Candidate
 
     object Ops:
       import DFVal.Ops.BoolOnlyOp
-      given evLogicOpDFBoolOrBit[
-          Op <: FuncOp.|.type | FuncOp.&.type | FuncOp.^.type,
-          L <: Candidate.Types,
-          LT <: DFBoolOrBit,
-          LP,
-          R <: Candidate.Types,
-          RT <: DFBoolOrBit,
-          RP
-      ](using
-          icL: Candidate.Aux[L, LT, LP],
-          icR: Candidate.Aux[R, RT, RP],
+      given evLogicOpDFBoolOrBit[Op <: FuncOp, L, LP, R, RP](using
+          icL: Candidate.Aux[L, LP],
+          icR: Candidate.Aux[R, RP],
           op: ValueOf[Op]
-      ): ExactOp2Aux[Op, DFC, DFValAny, L, R, DFValTP[LT, LP | RP]] = ???
-      given evLogicOpDFBoolOrBit2[
-          Op <: FuncOp.|.type | FuncOp.&.type,
-          L <: Candidate.Types,
-          R <: Candidate.Types,
-          O <: DFValAny
-      ](using
+      ): ExactOp2Aux[Op, DFC, DFValAny, L, R, DFValTP[DFTypeAny, LP | RP]] = ???
+      given evLogicOpDFBoolOrBit2[Op <: FuncOp, L, R, O <: DFValAny](using
           ic: ExactOp2Aux[Op, DFC, DFValAny, L, R, O]
       ): ExactOp2Aux[BoolOnlyOp[Op], DFC, DFValAny, L, R, O] = ???
 
