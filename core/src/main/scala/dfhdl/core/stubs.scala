484e5a33313c6bac72226365e31f34e3f9c84017
Oron Port <oron.port@dfiant.works>
2026-04-22 09:51:27 +0300

    wip reducing ExactOp2Aux to 3 params (removed B)

--- core/src/main/scala/dfhdl/core/stubs.scala
+++ core/src/main/scala/dfhdl/core/stubs.scala
@@ -1,6 +1,6 @@
 package dfhdl.core
 
-trait ExactOp2Aux[Op, C, B, O]
+trait ExactOp2Aux[Op, C, O]
 
 sealed class Modifier[+P]
 type ModifierAny = Modifier[Any]
@@ -16,8 +16,8 @@ object DFBoolOrBit:
     object Ops:
       import DFVal.Ops.BoolOnlyOp
       given bl[Op, O](using
-          ExactOp2Aux[Op, DFC, Any, O]
-      ): ExactOp2Aux[BoolOnlyOp, DFC, Any, O] = ???
+          ExactOp2Aux[Op, DFC, O]
+      ): ExactOp2Aux[BoolOnlyOp, DFC, O] = ???
 
 object DFDecimal:
   object Val:
@@ -29,11 +29,11 @@ object DFXInt:
     object Ops:
       type A = Int
       type B = String
-      given arith1[Op <: A]: ExactOp2Aux[Op, DFC, Any, DFValTP[DFType, Any]] = ???
-      given arith2[Op <: B]: ExactOp2Aux[Op, DFC, Any, DFValTP[DFType, Any]] = ???
+      given arith1[Op <: A]: ExactOp2Aux[Op, DFC, DFValTP[DFType, Any]] = ???
+      given arith2[Op <: B]: ExactOp2Aux[Op, DFC, DFValTP[DFType, Any]] = ???
 
       import DFVal.Ops.CarryOp
-      given c1: ExactOp2Aux[CarryOp, DFC, Any, DFValTP[DFType, Any]] = ???
-      given c2: ExactOp2Aux[CarryOp, DFC, Any, DFValTP[DFType, Any]] = ???
+      given c1: ExactOp2Aux[CarryOp, DFC, DFValTP[DFType, Any]] = ???
+      given c2: ExactOp2Aux[CarryOp, DFC, DFValTP[DFType, Any]] = ???
 
 type DFConstInt32 = DFConstOf[DFType]
