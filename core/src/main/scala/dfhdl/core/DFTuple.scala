bc56089748ef691646b44c4614eaf7334029b217
Oron Port <oron.port@dfiant.works>
2026-04-22 06:49:46 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFTuple.scala
+++ core/src/main/scala/dfhdl/core/DFTuple.scala
@@ -4,20 +4,5 @@ import dfhdl.internals.*
 import scala.annotation.unchecked.uncheckedVariance
 
 type DFTuple[+T <: NonEmptyTuple] = DFStruct[T @uncheckedVariance]
-object DFTuple:
-  private[core] def apply[T <: NonEmptyTuple](t: NonEmptyTuple)(using DFC): DFTuple[T] = ???
-  private[core] def apply[T <: NonEmptyTuple](fieldList: List[DFTypeAny])(using DFC): DFTuple[T] = ???
-  private[core] def unapply(t: NonEmptyTuple)(using DFC): Option[DFTuple[NonEmptyTuple]] = ???
+object DFTuple
 
-  trait TCZipper[T <: NonEmptyTuple, V <: NonEmptyTuple, O, TC[T <: DFTypeAny, V] <: TCCommon[T, V, O]]:
-    type OutP
-    def apply(fieldList: List[DFTypeAny], tupleValues: List[Any]): List[O]
-  object TCZipper
-
-  object Val:
-    private[core] def unapply(tuple: Tuple)(using DFC): Option[DFValOf[DFTuple[NonEmptyTuple]]] = ???
-    object TC
-    object TCConv
-    object Compare
-    object Ops
-end DFTuple
--- core/src/main/scala/dfhdl/core/TCCommon.scala
+++
@@ -1,5 +0,0 @@
-package dfhdl.core
-import dfhdl.internals.Exact1
-trait TCCommon[T <: DFTypeAny, V, O]
-    extends Exact1.TC[DFTypeAny, T, [t <: DFTypeAny] =>> t, V, DFC]:
-  type Out <: O
