a77a8754119ec8f2b11330031b24e672f5cd35c8
Oron Port <oron.port@dfiant.works>
2026-04-22 05:32:19 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFRef.scala
+++ core/src/main/scala/dfhdl/core/DFRef.scala
@@ -1,67 +1,13 @@
 package dfhdl.core
 import dfhdl.compiler.ir
-import dfhdl.compiler.analysis.DclPort
 
-import scala.annotation.targetName
 extension [M <: ir.DFMember](member: M)
-  private[dfhdl] def injectGlobalCtx()(using DFC): Unit =
-    import dfc.getSet
-    member match
-      case dfVal: ir.DFVal.CanBeGlobal if dfVal.isGlobal =>
-        given CanEqual[Any, Null] = CanEqual.derived
-        if (dfVal.globalCtx != null)
-          dfc.mutableDB.injectGlobals(
-            dfVal.globalCtx.asInstanceOf[DesignContext]
-          )
-      case _ =>
-  end injectGlobalCtx
-  // due to user meta-programming, it's possible that the user attempts to reference "unreachable"
-  // values within a certain design. this method attempts to create reachable members instead of
-  // limiting the user capabilities during elaboration.
-  private[core] def getReachableMember(using dfc: DFC): M =
-    import dfc.getSet
-    member match
-      case dcl: ir.DFVal.Dcl               => member
-      case pbns: ir.DFVal.PortByNameSelect => member
-      // only unreachable members are values, and we disable this mechanism during compiler stage
-      // meta-programming since there we reference values outside of the design context entirely.
-      case dfVal: ir.DFVal if !dfc.inMetaProgramming && !dfVal.isGlobal =>
-        dfc.ownerOption match
-          case Some(currentOwner) => dfVal.cloneUnreachable.asInstanceOf[M]
-          case _                  => member
-      case _ => member
-    end match
-  end getReachableMember
-
-  def ref(using DFC): ir.DFRef.OneWay[M] =
-    val newRef = dfc.refGen.genOneWay[M]
-    dfc.mutableDB.newRefFor(newRef, member)
-  def refTW[O <: ir.DFMember](using dfc: DFC): ir.DFRef.TwoWay[M, O] =
-    refTW[O](knownReachable = false)
-  def refTW[O <: ir.DFMember](knownReachable: Boolean)(using dfc: DFC): ir.DFRef.TwoWay[M, O] =
-    import dfc.getSet
-    injectGlobalCtx()
-    val reachableMember = if (knownReachable) member else member.getReachableMember
-    reachableMember match
-      // referencing a port from another design causes by-name referencing.
-      // in meta-programming we can end up with a modified copy of the design that should
-      // not be treated as a different design (for example, the stage `ToED`).
-      // for this reason we only compare the owner references which are guaranteed to be
-      // different for different design, but not for a copy made during meta-programming step.
-      case port @ DclPort()
-          if port.getOwnerDesign.ownerRef != dfc.owner.asIR.getThisOrOwnerDesign.ownerRef =>
-        // name path accounts for domains within the design that can contain the port
-        val namePath = port.getRelativeName(port.getOwnerDesign)
-        val portSelect = DFVal.PortByNameSelect(port.dfType, port.getOwnerDesign, namePath)
-        portSelect.refTW[O].asInstanceOf[ir.DFRef.TwoWay[M, O]]
-      // any other kind of reference
-      case member =>
-        val newRef = dfc.refGen.genTwoWay[M, O]
-        dfc.mutableDB.newRefFor(newRef, member)
-    end match
-  end refTW
+  private[dfhdl] def injectGlobalCtx()(using DFC): Unit = ???
+  private[core] def getReachableMember(using dfc: DFC): M = ???
+  def ref(using DFC): ir.DFRef.OneWay[M] = ???
+  def refTW[O <: ir.DFMember](using dfc: DFC): ir.DFRef.TwoWay[M, O] = ???
+  def refTW[O <: ir.DFMember](knownReachable: Boolean)(using dfc: DFC): ir.DFRef.TwoWay[M, O] = ???
 end extension
 
 extension [T <: ir.DFOwner](owner: DFOwner[T])
-  def ref(using DFC): ir.DFRef.OneWay[T] =
-    owner.asIR.ref
+  def ref(using DFC): ir.DFRef.OneWay[T] = ???
