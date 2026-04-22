b9ebd56ae7e238d032ca6527d211d26fe55c9671
Oron Port <oron.port@dfiant.works>
2026-04-22 08:09:26 +0300

    wip merging DFType into stubs

--- core/src/main/scala/dfhdl/core/stubs.scala
+++ core/src/main/scala/dfhdl/core/stubs.scala
@@ -49,6 +49,21 @@ end IntP
 
 export DFType.asFE
 
+sealed trait Args
+sealed trait NoArgs extends Args
+sealed trait Args1[T1] extends Args
+sealed trait Args4[T1, T2, T3, T4] extends Args
+
+final class DFType[+T <: ir.DFType, +A <: Args](val value: T) extends AnyVal
+type DFTypeAny = DFType[ir.DFType, Args]
+
+object DFType:
+  extension (dfType: ir.DFType) def asFE[T <: DFTypeAny]: T = ???
+  export DFBoolOrBit.given
+  export DFBits.given
+  export DFDecimal.given
+end DFType
+
 type DFBits[W <: IntP] = DFType[ir.DFBits, Args1[W]]
 object DFBits:
   given [W <: IntP & Singleton]: DFBits[W] = ???
