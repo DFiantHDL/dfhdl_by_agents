2eb72e7919e8c513cfb912a0f4f8f392be6ae303
Oron Port <oron.port@dfiant.works>
2026-04-22 08:10:03 +0300

    wip merging DFC into stubs

--- core/src/main/scala/dfhdl/core/stubs.scala
+++ core/src/main/scala/dfhdl/core/stubs.scala
@@ -1,4 +1,5 @@
 package dfhdl.core
+import scala.annotation.Annotation
 
 // stubs replacing dfhdl.compiler.ir and dfhdl.internals
 object ir:
@@ -68,3 +69,30 @@ type DFBits[W <: IntP] = DFType[ir.DFBits, Args1[W]]
 object DFBits:
   given [W <: IntP & Singleton]: DFBits[W] = ???
 end DFBits
+
+final case class DFC(
+    nameOpt: Option[String],
+    position: Position,
+    docOpt: Option[String],
+    mutableDB: MutableDB = new MutableDB()
+) extends MetaContext:
+  def setMeta(
+      nameOpt: Option[String] = nameOpt,
+      position: Position = position,
+      docOpt: Option[String] = docOpt,
+      annotations: List[Annotation] = Nil
+  ): this.type = ???
+  def setMeta(meta: ir.Meta): this.type = ???
+  def anonymize: this.type = ???
+  def setName(name: String): this.type = ???
+end DFC
+object DFC:
+  def emptyNoEO: DFC = ???
+end DFC
+
+into opaque type DFCG <: DFC = DFC
+protected trait DFCGLP:
+  inline given DFCG = DFCG()
+object DFCG extends DFCGLP:
+  def apply(): DFCG = DFC.emptyNoEO
+  given DFCG(using dfc: DFC): DFCG = dfc
