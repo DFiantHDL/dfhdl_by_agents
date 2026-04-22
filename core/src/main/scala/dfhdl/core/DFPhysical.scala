8fad780d98adfe1e530054988a16eae43d367ed2
Oron Port <oron.port@dfiant.works>
2026-04-22 05:57:28 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFPhysical.scala
+++ core/src/main/scala/dfhdl/core/DFPhysical.scala
@@ -1,16 +1,8 @@
 package dfhdl.core
 import dfhdl.compiler.ir
 import dfhdl.internals.*
-import scala.annotation.unchecked.uncheckedVariance
 import ir.DFVal.Func.Op as FuncOp
 import scala.annotation.targetName
-import scala.annotation.implicitNotFound
-
-extension (bd: BigDecimal.type)
-  private def apply(arg: Int | Long | Double): BigDecimal = arg match
-    case i: Int    => BigDecimal(i)
-    case l: Long   => BigDecimal(l)
-    case d: Double => BigDecimal(d)
 
 type DFPhysical[U <: ir.PhysicalNumber] = DFType[ir.DFPhysical[U], NoArgs]
 object DFPhysical:
@@ -27,27 +19,23 @@ object DFPhysical:
       new TC[DFValTP[DFPhysical[U], P]]:
         type OutU = U
         type OutP = P
-        def apply(value: DFValTP[DFPhysical[U], P])(using DFC): DFValTP[DFPhysical[U], P] = value
+        def apply(value: DFValTP[DFPhysical[U], P])(using DFC): DFValTP[DFPhysical[U], P] = ???
     given fromDFIntOrDFDouble[P]: TC[DFValTP[DFInt32 | DFDouble, P]] with
       type OutU = ir.LiteralNumber
       type OutP = P
-      def apply(value: DFValTP[DFInt32 | DFDouble, P])(using DFC): DFValTP[DFNumber, OutP] =
-        Val.Ops.toNumber(value)
+      def apply(value: DFValTP[DFInt32 | DFDouble, P])(using DFC): DFValTP[DFNumber, OutP] = ???
     given TC[Int] with
       type OutU = ir.LiteralNumber
       type OutP = CONST
-      def apply(value: Int)(using DFC): DFValTP[DFNumber, OutP] =
-        DFVal.Const(DFNumber, ir.LiteralNumber(BigDecimal(value)), named = true)
+      def apply(value: Int)(using DFC): DFValTP[DFNumber, OutP] = ???
     given TC[Long] with
       type OutU = ir.LiteralNumber
       type OutP = CONST
-      def apply(value: Long)(using DFC): DFValTP[DFNumber, OutP] =
-        DFVal.Const(DFNumber, ir.LiteralNumber(BigDecimal(value)), named = true)
+      def apply(value: Long)(using DFC): DFValTP[DFNumber, OutP] = ???
     given TC[Double] with
       type OutU = ir.LiteralNumber
       type OutP = CONST
-      def apply(value: Double)(using DFC): DFValTP[DFNumber, OutP] =
-        DFVal.Const(DFNumber, ir.LiteralNumber(BigDecimal(value)), named = true)
+      def apply(value: Double)(using DFC): DFValTP[DFNumber, OutP] = ???
   end TC
   object Val:
     object Ops:
@@ -57,21 +45,6 @@ object DFPhysical:
       object OpTC:
         type Aux[Op <: FuncOp, LU <: ir.PhysicalNumber, RU <: ir.PhysicalNumber, OU <: ir.PhysicalNumber] =
           OpTC[Op, LU, RU] { type OutU = OU }
-      extension (lhs: Int | Long | Double)
-        def fs(using DFCG): DFConstOf[DFTime] = ???
-        def ps(using DFCG): DFConstOf[DFTime] = ???
-        def ns(using DFCG): DFConstOf[DFTime] = ???
-        def us(using DFCG): DFConstOf[DFTime] = ???
-        def ms(using DFCG): DFConstOf[DFTime] = ???
-        def sec(using DFCG): DFConstOf[DFTime] = ???
-        def mn(using DFCG): DFConstOf[DFTime] = ???
-        def hr(using DFCG): DFConstOf[DFTime] = ???
-        def Hz(using DFCG): DFConstOf[DFFreq] = ???
-        def KHz(using DFCG): DFConstOf[DFFreq] = ???
-        def MHz(using DFCG): DFConstOf[DFFreq] = ???
-        def GHz(using DFCG): DFConstOf[DFFreq] = ???
-      end extension
-
       given evOpArithDFPhysical[
           Op <: FuncOp.+.type | FuncOp.-.type | FuncOp.*.type | FuncOp./.type,
           U <: ir.PhysicalNumber, LP, L <: DFValTP[DFPhysical[U], LP],
@@ -80,13 +53,6 @@ object DFPhysical:
           tcR: TC.Aux[R, RU, RP],
           tcOp: OpTC.Aux[Op, U, RU, OU]
       ): ExactOp2Aux[Op, DFC, DFValAny, L, R, DFValTP[DFPhysical[OU], LP | RP]] = ???
-
-      extension [LP](lhs: DFValTP[DFInt32 | DFDouble, LP])
-        def toNumber(using DFC): DFValTP[DFNumber, LP] = ???
-      extension [LP](lhs: DFValTP[DFNumber, LP])
-        @targetName("fromNumberToInt")
-        def toInt(using DFC): DFValTP[DFInt32, LP] = ???
-        def toDouble(using DFC): DFValTP[DFDouble, LP] = ???
     end Ops
   end Val
 end DFPhysical
