02f0756f2ea076c580fb26099290486cfbebf6a9
Oron Port <oron.port@dfiant.works>
2026-04-22 05:58:01 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/Width.scala
+++ core/src/main/scala/dfhdl/core/Width.scala
@@ -3,7 +3,6 @@ import dfhdl.internals.*
 import dfhdl.compiler.ir
 import scala.quoted.*
 import annotation.targetName
-import scala.annotation.nowarn
 
 trait Width[T]:
   type Out <: IntP
@@ -19,21 +18,9 @@ trait WidthLP:
 object Width extends WidthLP:
   type Aux[T, O <: IntP] = Width[T] { type Out = O }
   type AuxI[T, O <: Int] = Width[T] { type OutI = O }
-  val wide: Width[DFTypeAny] = new Width[DFTypeAny]:
-    type Out = Int
-    type OutI = Int
   given fromDFBoolOrBit[T <: DFBoolOrBit]: Width[T] with
     type Out = 1
     type OutI = 1
-  given fromBooleanCompanion: Width[Boolean.type] with
-    type Out = 1
-    type OutI = 1
-  given fromDFDouble: Width[DFDouble] with
-    type Out = 64
-    type OutI = 64
-  given fromDoubleCompanion: Width[Double.type] with
-    type Out = 64
-    type OutI = 64
   given fromDFBitsInt[W <: Int]: Width[DFBits[W]] with
     type Out = W
     type OutI = W
@@ -42,14 +29,6 @@ object Width extends WidthLP:
     type Out = W
     type OutI = W
   transparent inline given [T]: Width[T] = ${ getWidthMacro[T] }
-  extension (using quotes: Quotes)(dfTpe: quotes.reflect.TypeRepr)
-    def +(rhs: quotes.reflect.TypeRepr): quotes.reflect.TypeRepr = ???
-    def *(rhs: quotes.reflect.TypeRepr): quotes.reflect.TypeRepr = ???
-    infix def max(rhs: quotes.reflect.TypeRepr): quotes.reflect.TypeRepr = ???
-    def simplify: quotes.reflect.TypeRepr = ???
-    def calcWidth: quotes.reflect.TypeRepr = ???
-    def calcValWidth: quotes.reflect.TypeRepr = ???
-  end extension
   object Success extends Width[Any]
   def getWidthMacro[T](using Quotes, Type[T]): Expr[Width[T]] =
     '{ Success.asInstanceOf[Width[T] { type Out = Int; type OutI = Int }] }
