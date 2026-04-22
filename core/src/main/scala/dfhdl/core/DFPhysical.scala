5f677360a47116bde0ec9ac5c8d6ab939b18e687
Oron Port <oron.port@dfiant.works>
2026-04-22 06:34:25 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFPhysical.scala
+++ core/src/main/scala/dfhdl/core/DFPhysical.scala
@@ -6,55 +6,8 @@ import scala.annotation.targetName
 
 type DFPhysical[U <: ir.PhysicalNumber] = DFType[ir.DFPhysical[U], NoArgs]
 object DFPhysical:
-  given DFTime = DFTime
-  given DFFreq = DFFreq
-  given DFNumber = DFNumber
-  trait TC[-T]:
-    type OutU <: ir.PhysicalNumber
-    type OutP
-    def apply(value: T)(using DFC): DFValTP[DFPhysical[OutU], OutP]
-  object TC:
-    type Aux[T, U <: ir.PhysicalNumber, P] = TC[T] { type OutU = U; type OutP = P }
-    given [U <: ir.PhysicalNumber, P]: Aux[DFValTP[DFPhysical[U], P], U, P] =
-      new TC[DFValTP[DFPhysical[U], P]]:
-        type OutU = U
-        type OutP = P
-        def apply(value: DFValTP[DFPhysical[U], P])(using DFC): DFValTP[DFPhysical[U], P] = ???
-    given fromDFIntOrDFDouble[P]: TC[DFValTP[DFInt32 | DFDouble, P]] with
-      type OutU = ir.LiteralNumber
-      type OutP = P
-      def apply(value: DFValTP[DFInt32 | DFDouble, P])(using DFC): DFValTP[DFNumber, OutP] = ???
-    given TC[Int] with
-      type OutU = ir.LiteralNumber
-      type OutP = CONST
-      def apply(value: Int)(using DFC): DFValTP[DFNumber, OutP] = ???
-    given TC[Long] with
-      type OutU = ir.LiteralNumber
-      type OutP = CONST
-      def apply(value: Long)(using DFC): DFValTP[DFNumber, OutP] = ???
-    given TC[Double] with
-      type OutU = ir.LiteralNumber
-      type OutP = CONST
-      def apply(value: Double)(using DFC): DFValTP[DFNumber, OutP] = ???
-  end TC
   object Val:
-    object Ops:
-      trait OpTC[Op <: FuncOp, LU <: ir.PhysicalNumber, RU <: ir.PhysicalNumber]:
-        type OutU <: ir.PhysicalNumber
-        def apply(lhsDFType: DFPhysical[LU], rhsDFType: DFPhysical[RU]): DFPhysical[OutU]
-      object OpTC:
-        type Aux[Op <: FuncOp, LU <: ir.PhysicalNumber, RU <: ir.PhysicalNumber, OU <: ir.PhysicalNumber] =
-          OpTC[Op, LU, RU] { type OutU = OU }
-      given evOpArithDFPhysical[
-          Op <: FuncOp.+.type | FuncOp.-.type | FuncOp.*.type | FuncOp./.type,
-          U <: ir.PhysicalNumber, LP, L <: DFValTP[DFPhysical[U], LP],
-          R, RU <: ir.PhysicalNumber, RP, OU <: ir.PhysicalNumber
-      ](using
-          tcR: TC.Aux[R, RU, RP],
-          tcOp: OpTC.Aux[Op, U, RU, OU]
-      ): ExactOp2Aux[Op, DFC, DFValAny, L, R, DFValTP[DFPhysical[OU], LP | RP]] = ???
-    end Ops
-  end Val
+    object Ops
 end DFPhysical
 
 type DFTime = DFPhysical[ir.TimeNumber]
