ad0a08ab5867dd71e0a3518486e07bc6cc1ca5d9
Oron Port <oron.port@dfiant.works>
2026-04-22 03:18:47 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/Bubble.scala
+++ core/src/main/scala/dfhdl/core/Bubble.scala
@@ -1,13 +1,10 @@
 package dfhdl.core
-import dfhdl.compiler.ir
 
 sealed trait Bubble
 object Bubble extends Bubble:
   enum Behaviour derives CanEqual:
     case Stall, DontCare
   given Behaviour = Behaviour.Stall
-  def constValOf[T <: DFTypeAny](dfType: T, named: Boolean)(using dfc: DFC): DFConstOf[T] =
-    import dfc.getSet
-    DFVal.Const.forced(dfType, dfType.asIR.createBubbleData, named)
+  def constValOf[T <: DFTypeAny](dfType: T, named: Boolean)(using dfc: DFC): DFConstOf[T] = ???
 
 final val ? = Bubble
