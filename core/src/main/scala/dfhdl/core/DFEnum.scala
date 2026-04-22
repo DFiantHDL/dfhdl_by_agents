c566903b4fb6152f115d541594c47c458a286b8e
Oron Port <oron.port@dfiant.works>
2026-04-22 06:33:53 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFEnum.scala
+++ core/src/main/scala/dfhdl/core/DFEnum.scala
@@ -5,67 +5,14 @@ import scala.quoted.*
 import ir.DFVal.Func.Op as FuncOp
 import scala.annotation.targetName
 
-sealed abstract class DFEncoding extends scala.reflect.Enum:
-  def calcWidth(entryCount: Int): Int
-  def encode(idx: Int): BigInt
-  def bigIntValue: BigInt
-
-object DFEncoding:
-  sealed trait Auto extends DFEncoding:
-    final val bigIntValue: BigInt = ???
-  sealed trait ExplicitWidth[W <: Int & Singleton] extends DFEncoding:
-    val width: W
-  abstract class Default extends StartAt(0)
-
-  abstract class Gray extends Auto:
-    final def calcWidth(entryCount: Int): Int = ???
-    final def encode(idx: Int): BigInt = ???
-
-  abstract class StartAt[V <: Int & Singleton](value: V) extends Auto:
-    final def calcWidth(entryCount: Int): Int = ???
-    final def encode(idx: Int): BigInt = ???
-
-  abstract class OneHot extends Auto:
-    final def calcWidth(entryCount: Int): Int = ???
-    final def encode(idx: Int): BigInt = ???
-
-  abstract class Manual[W <: Int & Singleton](val width: W) extends ExplicitWidth[W]:
-    val value: DFConstOf[DFUInt[W]]
-    final def bigIntValue: BigInt = ???
-    final def calcWidth(entryCount: Int): Int = ???
-    final def encode(idx: Int): BigInt = ???
-
-  abstract class Toggle extends Default, ExplicitWidth[1] derives CanEqual:
-    val width: 1 = 1
-
-end DFEncoding
+sealed abstract class DFEncoding extends scala.reflect.Enum
 
 type DFEnum[E <: DFEncoding] = DFType[ir.DFEnum, Args1[E]]
 object DFEnum:
-  def unapply(using
-      Quotes
-  )(
-      tpe: quotes.reflect.TypeRepr
-  ): Option[List[quotes.reflect.TypeRepr]] = ???
-  def apply[E <: DFEncoding](enumCompanion: Object): DFEnum[E] = ???
-
-  inline given [E <: DFEncoding]: DFEnum[E] = ${ dfTypeMacro[E] }
-  def dfTypeMacro[E <: DFEncoding](using Quotes, Type[E]): Expr[DFEnum[E]] = ???
-
+  inline given [E <: DFEncoding]: DFEnum[E] = ???
   object Val:
-    object TC:
-      import DFVal.TC
-      given DFEnumFromEntry[E <: DFEncoding, RE <: E]: TC[DFEnum[E], RE] with
-        type OutP = CONST
-        def conv(dfType: DFEnum[E], value: RE)(using DFC): Out = ???
-    object Compare:
-      import DFVal.Compare
-      given DFEnumCompareEntry[
-          E <: DFEncoding, RE <: E,
-          Op <: FuncOp.===.type | FuncOp.=!=.type, C <: Boolean
-      ]: Compare[DFEnum[E], RE, Op, C] with
-        type OutP = CONST
-        def conv(dfType: DFEnum[E], arg: RE)(using DFC): Out = ???
+    object TC
+    object Compare
     object Ops
   end Val
 end DFEnum
