a5a51c5897e93a9b26745b0cb77fa6764354cb05
Oron Port <oron.port@dfiant.works>
2026-04-22 10:07:14 +0300

    wip moving CarryOp import to top level

--- core/src/main/scala/dfhdl/core/stubs.scala
+++ core/src/main/scala/dfhdl/core/stubs.scala
@@ -20,14 +20,13 @@ object DFDecimal:
   object Ops:
     export DFXInt.Ops.*
 
+import DFVal.Ops.CarryOp
 object DFXInt:
   object Ops:
     type A = Int
     type B = String
     given arith1[Op <: A]: ExactOp2Aux[Op, DFC, DFValTP[DFType]] = ???
     given arith2[Op <: B]: ExactOp2Aux[Op, DFC, DFValTP[DFType]] = ???
-
-    import DFVal.Ops.CarryOp
     given c1: ExactOp2Aux[CarryOp, DFC, DFValTP[DFType]] = ???
     given c2: ExactOp2Aux[CarryOp, DFC, DFValTP[DFType]] = ???
 
