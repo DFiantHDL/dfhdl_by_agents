598bc1bf8200e96cde0bf41ffbf8f39c9d724d4f
Oron Port <oron.port@dfiant.works>
2026-04-22 07:01:59 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/IntParam.scala
+++ core/src/main/scala/dfhdl/core/IntParam.scala
@@ -1,21 +1,6 @@
 package dfhdl.core
-import dfhdl.compiler.ir
-import ir.DFVal.Func.Op as FuncOp
-import ir.DFDecimal.NativeType
-import compiletime.ops.int
-import int.*
-import compiletime.{constValueOpt, constValue}
-import dfhdl.internals.Inlined
-import scala.annotation.targetName
-
 type IntP = Int | DFConstInt32
 object IntP:
   type +[L <: IntP, R <: IntP] = Int
   type Max[L <: IntP, R <: IntP] = Int
 end IntP
-
-into opaque type IntParam[V <: IntP] = Int | DFConstInt32
-object IntParam:
-  inline implicit def fromValue[T <: IntP & Singleton](inline value: T): IntParam[T] =
-    value.asInstanceOf[IntParam[T]]
-end IntParam
