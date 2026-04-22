5101945317989b7f329156e346055eb868f991df
Oron Port <oron.port@dfiant.works>
2026-04-22 06:32:30 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/IntParam.scala
+++ core/src/main/scala/dfhdl/core/IntParam.scala
@@ -53,48 +53,7 @@ object IntP:
 end IntP
 
 into opaque type IntParam[V <: IntP] = Int | DFConstInt32
-protected sealed trait IntParamLP:
-  given [T <: IntP]: Conversion[IntParam[T], IntParam[Int]] = value =>
-    value.asInstanceOf[IntParam[Int]]
-object IntParam extends IntParamLP:
-  given [L <: IntP, R <: IntP](using CanEqual[L, R]): CanEqual[IntParam[L], IntParam[R]] =
-    CanEqual.derived
-  given [T <: IntP]: CanEqual[IntParam[T], Int] = CanEqual.derived
-  given [T <: IntP]: CanEqual[Int, IntParam[T]] = CanEqual.derived
-
+object IntParam:
   inline implicit def fromValue[T <: IntP & Singleton](inline value: T): IntParam[T] =
     value.asInstanceOf[IntParam[T]]
-  @targetName("fromValueInlined")
-  inline implicit def fromValue[T <: Int](inline value: Inlined[T]): IntParam[T] =
-    value.asInstanceOf[IntParam[T]]
-  @targetName("fromValueWide")
-  inline implicit def fromValue[Wide <: IntP](inline value: Wide): IntParam[Wide] =
-    value.asInstanceOf[IntParam[Wide]]
-  inline def apply[T <: IntP](inline value: T): IntParam[T] = value match
-    case sig: IntP.Sig => sig.value.asInstanceOf[IntParam[T]]
-    case _             => value.asInstanceOf[IntParam[T]]
-  inline def forced[V <: IntP](inline value: IntP): IntParam[V] = value.asInstanceOf[IntParam[V]]
-  @targetName("applyInlined")
-  inline def apply[V <: Int](inline value: Inlined[V]): IntParam[V] =
-    value.asInstanceOf[IntParam[V]]
-  extension [L <: IntP](lhs: IntParam[L])(using dfc: DFC)
-    def toDFConst: DFConstInt32 = ???
-    def toScalaIntOpt: Option[Int] = ???
-    def toScalaIntUNSAFE: Int = ???
-    def ref: ir.IntParamRef = ???
-    def +[R <: IntP](rhs: IntParam[R]): IntParam[IntP.+[L, R]] = ???
-    def -[R <: IntP](rhs: IntParam[R]): IntParam[IntP.-[L, R]] = ???
-    def *[R <: IntP](rhs: IntParam[R]): IntParam[IntP.*[L, R]] = ???
-    def /[R <: IntP](rhs: IntParam[R]): IntParam[IntP./[L, R]] = ???
-    def %[R <: IntP](rhs: IntParam[R]): IntParam[IntP.%[L, R]] = ???
-    infix def max[R <: IntP](rhs: IntParam[R]): IntParam[IntP.Max[L, R]] = ???
-    infix def min[R <: IntP](rhs: IntParam[R]): IntParam[IntP.Min[L, R]] = ???
-    def clog2: IntParam[IntP.CLog2[L]] = ???
-    def =~[R <: IntP](that: IntParam[R]): Boolean = ???
-    protected[dfhdl] def cloneAnonValueAndDepsHere: IntParam[Int] = ???
-  end extension
 end IntParam
-
-extension (intParamRef: ir.IntParamRef)
-  def get(using dfc: DFC): IntParam[Int] = ???
-  protected[core] def refCodeString(using dfc: DFC): String = ???
