55d700ce62922c13671ea4b410b6244937fb2b1b
Oron Port <oron.port@dfiant.works>
2026-04-22 06:27:42 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/hdl.scala
+++ core/src/main/scala/dfhdl/hdl.scala
@@ -1,27 +1,8 @@
 package dfhdl
 protected object hdl:
-  class dsn extends scala.annotation.StaticAnnotation
-  import core.IntP
   export core.DFBoolOrBit.Val.Ops.*
-  export core.DFDecimal.StrInterpOps.{d, sd}
   export core.DFDecimal.Val.Ops.*
-  export core.DFEnum.Val.Ops.*
-  export core.DFVector.Val.Ops.*
-  export core.DFOpaque.Val.Ops.*
-  export core.DFTuple.Val.Ops.*
-  export core.DFVector.Ops.*
-  export core.TDFDouble.Val.Ops.*
-  export core.TDFString.Val.Ops.*
-  export core.DFVal.Ops.*
-  export core.DFVarOps.*
-  export core.ConnectOps.*
-  export core.Conditional.Ops.*
-  export core.TextOut.Ops.*
-  export compiler.ir.TextOut.Severity
   export internals.CommonOps.*
-  export core.{dfType}
-  export core.DFPhysical.Val.Ops.*
-  export core.LoopOps.*
 end hdl
 
 export hdl.*
