5c0cc63d548ea6adcdde8107a543cd84c5af2242
Oron Port <oron.port@dfiant.works>
2026-04-22 03:41:50 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFOpaque.scala
+++ core/src/main/scala/dfhdl/core/DFOpaque.scala
@@ -1,190 +1,43 @@
 package dfhdl.core
 import dfhdl.compiler.ir
-import dfhdl.compiler.printing.{DefaultPrinter, Printer}
 import dfhdl.internals.*
-import scala.quoted.*
-
 import scala.annotation.unchecked.uncheckedVariance
-import scala.annotation.targetName
 
-type DFOpaque[+TFE <: DFOpaque.Abstract] =
-  DFType[ir.DFOpaque, Args1[TFE @uncheckedVariance]]
+type DFOpaque[+TFE <: DFOpaque.Abstract] = DFType[ir.DFOpaque, Args1[TFE @uncheckedVariance]]
 object DFOpaque:
   protected[core] sealed trait Abstract extends HasTypeName, Product, Serializable:
     type ActualType <: DFTypeAny
     protected[core] val actualType: ActualType
   object Abstract:
     type Aux[TFE <: Abstract, AT <: DFTypeAny] = TFE { type ActualType = AT }
-    inline implicit def fromComp[TFE <: Abstract, Comp <: Object](tfeComp: Comp)(implicit
-        cc: CaseClass[Comp, TFE]
-    ): cc.CC = compiletime.summonInline[ClassEv[cc.CC]].value
-    def fromCompMacro[TFE <: Abstract, Comp <: Object](using
-        Quotes,
-        Type[TFE],
-        Type[Comp]
-    ): Expr[TFE] =
-      import quotes.reflect.*
-      val compTpe = TypeRepr.of[Comp]
-      val tfeTpe = compTpe.getCompanionClassTpe
-      val tfeType = tfeTpe.asTypeOf[TFE]
-      '{
-        compiletime.summonInline[ClassEv[tfeType.Underlying]].value
-      }
-  end Abstract
-
   abstract class Frontend[A <: DFTypeAny](final protected[core] val actualType: A) extends Abstract:
     type ActualType = A
-
   abstract class Magnet[A <: DFTypeAny](actualType: A) extends Frontend[A](actualType)
   abstract class Clk extends Magnet[DFBit](DFBit)
   abstract class Rst extends Magnet[DFBit](DFBit)
-
-  given [TFE <: Abstract](using ce: ClassEv[TFE], dfc: DFCG): DFOpaque[TFE] = DFOpaque(ce.value)
-
-  def apply[TFE <: Abstract](
-      t: TFE
-  )(using dfc: DFCG): DFOpaque[TFE] = trydf:
-    val kind = t match
-      case _: Clk       => ir.DFOpaque.Kind.Clk
-      case _: Rst       => ir.DFOpaque.Kind.Rst
-      case _: Magnet[?] => ir.DFOpaque.Kind.Magnet
-      case _            => ir.DFOpaque.Kind.General
-    val id = t match
-      case _: Magnet[?] => dfc.refGen.getMagnetID(t)
-      case _            =>
-        // Generate a stable ID based on the fully qualified class name
-        // This ensures different case classes have different IDs even if they have the same simple name
-        // but are in different packages, and remains stable between runs
-        val fullyQualifiedClassName = t.getClass.getName
-        fullyQualifiedClassName.hashCode
-    ir.DFOpaque(
-      t.typeName,
-      kind,
-      id,
-      t.actualType.asIR.dropUnreachableRefs(allowDesignParamRefs = false)
-    ).asFE[DFOpaque[TFE]]
+  given [TFE <: Abstract](using ce: ClassEv[TFE], dfc: DFCG): DFOpaque[TFE] = ???
+  def apply[TFE <: Abstract](t: TFE)(using dfc: DFCG): DFOpaque[TFE] = ???
   extension [A <: DFTypeAny, TFE <: Frontend[A]](dfType: DFOpaque[TFE])
-    def actualType: A = dfType.asIR.actualType.asFE[A]
-    def opaqueType: TFE = dfType.asIR.id.asInstanceOf[TFE]
-
+    def actualType: A = ???
+    def opaqueType: TFE = ???
   object Val:
-    object TC:
-      import DFVal.TC
-      given DFOpaqueValFromDFOpaqueVal[
-          TFE <: Abstract,
-          RT <: Abstract,
-          RP,
-          V <: DFValTP[DFOpaque[RT], RP]
-      ](using RT <:< TFE): TC[DFOpaque[TFE], V] with
-        type OutP = RP
-        def conv(dfType: DFOpaque[TFE], value: V)(using DFC): Out =
-          value.asValTP[DFOpaque[TFE], RP]
-
+    object TC
     object Ops:
-      given evOpAsDFOpaqueComp[
-          L,
-          Comp <: Object,
-          TFE <: Abstract
-      ](using
+      given evOpAsDFOpaqueComp[L, Comp <: Object, TFE <: Abstract](using
           cc: CaseClass.Aux[Comp, Abstract, TFE]
-      )(using
-          ce: ClassEv[TFE]
-      )(using
+      )(using ce: ClassEv[TFE])(using
           tc: DFVal.TC[ce.value.ActualType, L]
-      ): ExactOp2Aux["as", DFC, DFValAny, L, Comp, DFValTP[DFOpaque[TFE], tc.OutP]] =
-        new ExactOp2["as", DFC, DFValAny, L, Comp]:
-          type Out = DFValTP[DFOpaque[TFE], tc.OutP]
-          def apply(lhs: L, tfeComp: Comp)(using DFC): Out = trydf {
-            DFVal.Alias.AsIs(DFOpaque[TFE](ce.value), tc(ce.value.actualType, lhs))
-          }(using dfc, CTName("cast as opaque"))
-      end evOpAsDFOpaqueComp
-
-      given evOpAsDFOpaqueTFE[
-          L,
-          T <: DFTypeAny,
-          TFE <: Frontend[T]
-      ](using
+      ): ExactOp2Aux["as", DFC, DFValAny, L, Comp, DFValTP[DFOpaque[TFE], tc.OutP]] = ???
+      given evOpAsDFOpaqueTFE[L, T <: DFTypeAny, TFE <: Frontend[T]](using
           tc: DFVal.TC[T, L]
-      ): ExactOp2Aux["as", DFC, DFValAny, L, TFE, DFValTP[DFOpaque[TFE], tc.OutP]] =
-        new ExactOp2["as", DFC, DFValAny, L, TFE]:
-          type Out = DFValTP[DFOpaque[TFE], tc.OutP]
-          def apply(lhs: L, tfe: TFE)(using DFC): Out = trydf {
-            DFVal.Alias.AsIs(DFOpaque[TFE](tfe), tc(tfe.actualType, lhs))
-          }(using dfc, CTName("cast as opaque"))
-      end evOpAsDFOpaqueTFE
-
-      given evOpAsDFOpaqueIterable[
-          A <: DFTypeAny,
-          P,
-          L <: Iterable[DFValTP[A, P]],
-          T <: DFTypeAny,
-          TFE <: Frontend[T]
-      ](using
+      ): ExactOp2Aux["as", DFC, DFValAny, L, TFE, DFValTP[DFOpaque[TFE], tc.OutP]] = ???
+      given evOpAsDFOpaqueIterable[A <: DFTypeAny, P, L <: Iterable[DFValTP[A, P]], T <: DFTypeAny, TFE <: Frontend[T]](using
           tc: DFVal.TC[T, DFValTP[DFVector[A, Tuple1[Int]], P]]
-      ): ExactOp2Aux["as", DFC, DFValAny, L, TFE, DFValTP[DFOpaque[TFE], tc.OutP]] =
-        new ExactOp2["as", DFC, DFValAny, L, TFE]:
-          type Out = DFValTP[DFOpaque[TFE], tc.OutP]
-          def apply(lhs: L, tfe: TFE)(using DFC): Out = trydf {
-            DFVal.Alias.AsIs(DFOpaque[TFE](tfe), tc(tfe.actualType, asDFVector(lhs)))
-          }(using dfc, CTName("cast as opaque"))
-      end evOpAsDFOpaqueIterable
-
-      given evOpClkAsClkComp[
-          LTFE <: Clk,
-          L <: DFValOf[DFOpaque[LTFE]],
-          Comp <: Object,
-          TFE <: Clk
-      ](using
+      ): ExactOp2Aux["as", DFC, DFValAny, L, TFE, DFValTP[DFOpaque[TFE], tc.OutP]] = ???
+      given evOpClkAsClkComp[LTFE <: Clk, L <: DFValOf[DFOpaque[LTFE]], Comp <: Object, TFE <: Clk](using
           cc: CaseClass.Aux[Comp, Clk, TFE]
-      )(using
-          ce: ClassEv[TFE]
-      ): ExactOp2Aux["as", DFC, DFValAny, L, Comp, DFValOf[DFOpaque[TFE]]] =
-        new ExactOp2["as", DFC, DFValAny, L, Comp]:
-          type Out = DFValOf[DFOpaque[TFE]]
-          def apply(lhs: L, tfeComp: Comp)(using DFC): Out = trydf {
-            DFVal.Alias.AsIs(DFOpaque[TFE](ce.value), lhs)
-          }(using dfc, CTName("cast clk as a different clk"))
-      end evOpClkAsClkComp
-
-      given evOpRstAsRstComp[
-          LTFE <: Rst,
-          L <: DFValOf[DFOpaque[LTFE]],
-          Comp <: Object,
-          TFE <: Rst
-      ](using
+      )(using ce: ClassEv[TFE]): ExactOp2Aux["as", DFC, DFValAny, L, Comp, DFValOf[DFOpaque[TFE]]] = ???
+      given evOpRstAsRstComp[LTFE <: Rst, L <: DFValOf[DFOpaque[LTFE]], Comp <: Object, TFE <: Rst](using
           cc: CaseClass.Aux[Comp, Rst, TFE]
-      )(using
-          ce: ClassEv[TFE]
-      ): ExactOp2Aux["as", DFC, DFValAny, L, Comp, DFValOf[DFOpaque[TFE]]] =
-        new ExactOp2["as", DFC, DFValAny, L, Comp]:
-          type Out = DFValOf[DFOpaque[TFE]]
-          def apply(lhs: L, tfeComp: Comp)(using DFC): Out = trydf {
-            DFVal.Alias.AsIs(DFOpaque[TFE](ce.value), lhs)
-          }(using dfc, CTName("cast rst as a different rst"))
-      end evOpRstAsRstComp
-
-      private def asDFVector[A <: DFTypeAny, P](dfVals: Iterable[DFValTP[A, P]])(using
-          DFC
-      ): DFValTP[DFVector[A, Tuple1[Int]], P] =
-        val dfType = DFVector[A, Tuple1[Int]](dfVals.head.dfType, List(dfVals.size))
-        DFVal.Func(dfType, DFVal.Func.Op.++, dfVals.toList)
-
-      extension [AT <: DFTypeAny, TFE <: Frontend[AT], A, P](
-          lhs: DFVal[DFOpaque[TFE], Modifier[A, Any, Any, P]]
-      )
-        def opaqueType(using ce: ClassEv[TFE]): TFE = ce.value
-        def actual(using DFCG): DFVal[AT, Modifier[A, Any, Any, P]] = trydf {
-          DFVal.Alias.AsIs(lhs.dfType.actualType, lhs)
-        }
-        def mapActual(
-            f: DFValOf[AT] => DFValOf[AT]
-        )(using dfc: DFC, ce: ClassEv[TFE]): DFValOf[DFOpaque[TFE]] =
-          DFVal.Alias.AsIs(
-            DFOpaque[TFE](ce.value),
-            f(lhs.actual)
-          )
-      end extension
-    end Ops
-  end Val
-
+      )(using ce: ClassEv[TFE]): ExactOp2Aux["as", DFC, DFValAny, L, Comp, DFValOf[DFOpaque[TFE]]] = ???
 end DFOpaque
