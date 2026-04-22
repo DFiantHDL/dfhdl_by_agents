33e4e13646f737825fddb75893156e09314a38b9
Oron Port <oron.port@dfiant.works>
2026-04-22 07:11:15 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/DFVal.scala
+++ core/src/main/scala/dfhdl/core/DFVal.scala
@@ -1,22 +1,11 @@
 package dfhdl.core
 import dfhdl.compiler.ir
-import dfhdl.internals.*
 import ir.DFVal.Func.Op as FuncOp
-import ir.DFVal.Alias.History.Op as HistoryOp
-import ir.DFDecimal.NativeType
-
-import scala.annotation.unchecked.uncheckedVariance
-import scala.annotation.{implicitNotFound, targetName}
 import scala.quoted.*
-import dfhdl.compiler.ir.MemberGetSet
-import scala.annotation.tailrec
 
-import scala.reflect.ClassTag
 into final class DFVal[+T <: DFTypeAny, +M <: ModifierAny](val irValue: ir.DFVal)
-end DFVal
 
 type DFValAny = DFVal[DFTypeAny, ModifierAny]
-type DFConstAny = DFVal[DFTypeAny, Modifier.CONST]
 type DFValOf[+T <: DFTypeAny] = DFVal[T, ModifierAny]
 type DFConstOf[+T <: DFTypeAny] = DFVal[T, Modifier.CONST]
 type DFValTP[+T <: DFTypeAny, +P] = DFVal[T, Modifier[Any, Any, Any, P]]
@@ -24,29 +13,14 @@ type DFValTP[+T <: DFTypeAny, +P] = DFVal[T, Modifier[Any, Any, Any, P]]
 inline def isConstCheck[T]: Boolean = ${ isConstCheckMacro[T] }
 def isConstCheckMacro[T](using Quotes, Type[T]): Expr[Boolean] = ???
 
-type JUSTVAL[T] = DFValOf[DFTypeAny]
-
-
 object DFVal:
-  inline def apply[T <: DFTypeAny, M <: ModifierAny, IR <: ir.DFVal](
-      irValue: IR
-  ): DFVal[T, M] = new DFVal[T, M](irValue)
-
   export DFXInt.Val.Ops.{
     evOpCarryAddSubDFXInt,
     evOpCarryMulDFXInt
   }
-
-  object Const:
-    def apply(args: Any*)(using DFC): DFConstOf[DFTypeAny] = ???
-
-  trait TC[T <: DFTypeAny, R]:
-    type OutP
-
   object Ops:
     protected[core] trait BoolOnlyOp[Op <: FuncOp]
     protected[core] trait CarryOp[Op <: FuncOp]
-  end Ops
 end DFVal
 
 
