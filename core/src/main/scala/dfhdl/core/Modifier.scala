7f3ce622ff497eae27cdaa1ce50aba448a800c39
Oron Port <oron.port@dfiant.works>
2026-04-22 07:12:17 +0300

    wip minimizing

--- core/src/main/scala/dfhdl/core/Modifier.scala
+++ core/src/main/scala/dfhdl/core/Modifier.scala
@@ -1,10 +1,5 @@
 package dfhdl.core
-
 sealed class Modifier[+A, +C, +I, +P]
 type ModifierAny = Modifier[Any, Any, Any, Any]
 object Modifier:
-  type CONST = Modifier[Any, Any, Any, dfhdl.core.CONST]
-end Modifier
-
-sealed trait ISCONST[T <: Boolean]
-type CONST = ISCONST[true]
+  type CONST = Modifier[Any, Any, Any, Any]
